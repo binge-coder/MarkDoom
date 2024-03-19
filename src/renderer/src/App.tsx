795ebd8463b87b18982bc6e023a85fe4f38f5888
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2024-03-19 13:50:48 +0530

    before draggable testing

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,13 @@
+import { Content, RootLayout, Sidebar } from "./components"
+
 function App(): JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <div className="border border-red-500 h-full flex justify-center items-center">
-      <span>yo</span>
-    </div>
+<RootLayout>
+  <Sidebar className="bg-zinc-500/30 p-2">sidebar</Sidebar>
+  <Content className="border-l border-l-white/20 bg-zinc-500/40 "> content </Content>
+</RootLayout>
   )
 }
 
