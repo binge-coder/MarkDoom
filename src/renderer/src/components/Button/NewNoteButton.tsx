d6dc3a0734d35702e6e7fe145f12143e14472294
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2024-07-09 10:54:20 +0530

    before implementing save file system

--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,10 +1,16 @@
 import { ActionButton, ActionButtonProps } from "@/components";
+import { createEmptyNoteAtom } from "@/store";
+import { useSetAtom } from "jotai";
 import { LuFileSignature } from "react-icons/lu";
 // import ActionButton from "@/components/Button/ActionButton";
 
 export const NewNoteButton = (...props: ActionButtonProps[]) => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom);
+  const handleCreation = () => {
+    createEmptyNote();
+  };
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreation} {...props}>
       <LuFileSignature className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   );
