343d47b5361d6c98c429cd143b1251894197249c
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2024-07-24 15:56:16 +0530

    added types

--- src/renderer/src/components/PreferencesPage.tsx
+++ src/renderer/src/components/PreferencesPage.tsx
@@ -1,19 +1,28 @@
 // import React from 'react';
-import { useState } from "react";
-import fse from "fs-extra";
-import path from "path";
-const PrefListItem = ({ children, ...props }) => {
+import { PropsWithChildren } from "react";
+import React, { useState } from "react";
+const PrefListItem: React.FC<PropsWithChildren> = ({ children, ...props }) => {
   return (
     <div className="border border-slate-400 rounded-md p-1 mb-1 flex justify-between">
       {children}
     </div>
   );
 };
+type Props = {
+  isVisible: boolean;
+  onClose: () => void;
+};
 
-export const PreferencesPage = ({ isVisible, onClose }) => {
+export const PreferencesPage: React.FC<Props> = ({ isVisible, onClose }) => {
   if (!isVisible) return null;
+  //   const savedValue = window.settings.get("geminiApiKey");
+  //   const geminiApi = savedValue || "";
   const [geminiKeyInput, setgeminiKeyInput] = useState("");
 
+  const handleApiInput = (e) => {
+    setgeminiKeyInput(e.target.value);
+    // window.settings.set("geminiApiKey", e.target.value);
+  };
   return (
     <div className="fixed inset-10 bg-white rounded-md shadow-lg text-black">
       <button
@@ -31,7 +40,7 @@ export const PreferencesPage = ({ isVisible, onClose }) => {
             className="border border-black min-w-96 px-1"
             placeholder="paste your key"
             value={geminiKeyInput}
-            onChange={(e) => setgeminiKeyInput(e.target.value)}
+            onChange={handleApiInput}
           />
         </PrefListItem>
       </div>
