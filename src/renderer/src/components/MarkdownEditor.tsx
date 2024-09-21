8678bf42480195da7ce5454380df96d9d468044f
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2024-09-21 07:19:38 +0530

    completely fixed bullet points bad color

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -27,7 +27,7 @@ export const MarkdownEditor = () => {
         thematicBreakPlugin(),
         markdownShortcutPlugin(),
       ]}
-      contentEditableClassName="outline-none min-h-screen max-w-none text-lg px-8 py-5 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-200 prose-code:before:content-[''] prose-code:after:content-[''] prose-ol:" //add prose-invert for dark mode
+      contentEditableClassName="outline-none min-h-screen max-w-none px-8 py-5 caret-yellow-500 prose text-lg prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-200 prose-code:before:content-[''] prose-code:after:content-[''] prose-ul:prose prose-ul:text-slate-300 prose-ul:text-lg " //add prose-invert for dark mode
     />
   );
 };
