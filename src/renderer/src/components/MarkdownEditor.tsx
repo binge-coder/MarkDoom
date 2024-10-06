eacb99da2eef855c5493dec8ee3c590dbc9c2ac5
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2024-10-06 18:54:00 +0530

    links are now underlined

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -5,6 +5,8 @@ import {
   MDXEditor,
   quotePlugin,
   thematicBreakPlugin,
+  linkPlugin,
+  linkDialogPlugin,
 } from "@mdxeditor/editor";
 import { useMarkdownEditor } from "@renderer/hooks/useMarkdownEditor";
 
@@ -26,6 +28,8 @@ export const MarkdownEditor = () => {
         quotePlugin(),
         thematicBreakPlugin(),
         markdownShortcutPlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
       ]}
       contentEditableClassName="outline-none min-h-screen max-w-none px-8 py-5 caret-yellow-500 prose text-lg prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-200 prose-code:before:content-[''] prose-code:after:content-[''] prose-ul:prose prose-ul:text-slate-300 prose-ul:text-lg " //add prose-invert for dark mode
     />
