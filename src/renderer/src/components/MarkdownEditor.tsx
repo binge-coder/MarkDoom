2241f1909619eaba96872be73e6fe3b3099f0803
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2024-10-06 19:22:50 +0530

    height of editor div fixed

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -1,12 +1,12 @@
 import {
   headingsPlugin,
+  imagePlugin,
+  linkPlugin,
   listsPlugin,
   markdownShortcutPlugin,
   MDXEditor,
   quotePlugin,
   thematicBreakPlugin,
-  linkPlugin,
-  linkDialogPlugin,
 } from "@mdxeditor/editor";
 import { useMarkdownEditor } from "@renderer/hooks/useMarkdownEditor";
 
@@ -29,9 +29,9 @@ export const MarkdownEditor = () => {
         thematicBreakPlugin(),
         markdownShortcutPlugin(),
         linkPlugin(),
-        linkDialogPlugin(),
+        // imagePlugin({ imageUploadHandler }),
       ]}
-      contentEditableClassName="outline-none min-h-screen max-w-none px-8 py-5 caret-yellow-500 prose text-lg prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-200 prose-code:before:content-[''] prose-code:after:content-[''] prose-ul:prose prose-ul:text-slate-300 prose-ul:text-lg " //add prose-invert for dark mode
+      contentEditableClassName="outline-none  max-w-none px-8 py-5 caret-yellow-500 prose text-lg prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-200 prose-code:before:content-[''] prose-code:after:content-[''] prose-ul:prose prose-ul:text-slate-300 prose-ul:text-lg " //add prose-invert for dark mode
     />
   );
 };
