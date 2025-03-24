98dce8d0a55a6a5664057f11254a26efc97abb9f
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2025-03-24 08:37:58 +0530

    trying to fix gh action error

---
+++ src/react-icons-lu.d.ts
@@ -0,0 +1,7 @@
+import { IconType } from "react-icons";
+
+// Custom declaration for react-icons/lu to include LuFileSignature
+declare module "react-icons/lu" {
+  export const LuFileSignature: IconType;
+  // ...other exports...
+}
