006a4e8777b8de6358d969af7c6c860679629d37
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2025-05-05 13:35:46 +0530

    update mdxeditor

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
 export const appDirectoryName = "MarkDoom";
 export const fileEncoding = "utf8";
-export const autoSavingTime = 3000; //milliseconds, so 3 sec
+export const autoSavingTime = 1000; //in milliseconds
 export const welcomeNoteFileName = "Welcome.md";
